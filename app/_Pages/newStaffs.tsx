212b77a3e84628645dcc27ae440b4514631c5acf
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-02 23:01:57 +0500

    hm

--- app/_Pages/newStaffs.tsx
+++ app/_Pages/newStaffs.tsx
@@ -4,5 +4,13 @@ function newStaffs() {
     <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
   )
 }
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
 
 export default newStaffs
