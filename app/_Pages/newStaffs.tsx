7aecaa1c6c23dc15b8bee6e06fc917f8a45936ab
Waqar-ahmedkhan <waqarahmed44870@gmail.com>
2024-08-04 21:58:43 +0500

    new

--- app/_Pages/newStaffs.tsx
+++ app/_Pages/newStaffs.tsx
@@ -69,3 +69,500 @@ export default newStaffs
  export default newStaffs
 
 export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+
+function newStaffs() {
+  return (
+    <div>newStaffs Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis iusto in iure quod earum? Repudiandae, laborum, vitae quam nulla expedita vero porro molestias illum impedit quae iusto repellat libero ad?</div>
+  )
+}
+  
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
+ function newStaffs() {
+   return (
+     <div>newStaffs</div>
+   )
+ }
+ 
+ export default newStaffs
+
+export default newStaffs
